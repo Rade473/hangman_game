87f35d175e65b6676576f936cbbad31ac26c166c
Rade473 <r.shekularats@gmail.com>
2022-08-19 14:21:37 +0200

    Can save games now

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,16 @@
+#Game logic and game instances happen here
 class Game
 
 def initialize
-  @key = generate_key()
+  @key = generate_key.split('')
   @clues = []
+  @guess = Array.new(@key.length, '_')
+  @tried_letters = []
   @lives = 10
+  @display = []
   p @key
+  puts @guess.join('')
+  player_choice
 
 end
 
@@ -18,5 +24,58 @@ def generate_key
     end
   end
 filtereddictionary.sample
+end
+
+def player_choice
+  until @guess == @key || @lives <= 0
+    puts "You have #{@lives} tries left"
+    userinput = gets.chomp.downcase
+    return 'save' if userinput == 'save'
+    if userinput.length == 1 && ('a'..'z').include?(userinput)
+      try_letter(userinput)
+      compare_guess(userinput)
+    else
+      puts 'Invalid input, please enter a letter'
+      print '> '
+    end
+  end
+end
+
+def compare_guess(input)
+  @key.each_with_index do |letter, i|
+    if letter == input
+      @guess[i] = @key[i]
+    end
+  end
+  puts @guess.join('')
+end
+
+def try_letter(input)
+  if @tried_letters.include?(input)
+   puts "You already tried that letter"
+  elsif @key.include?(input)
+    @tried_letters.push(input)
+    display_tries(@tried_letters)
+  else 
+    @tried_letters.push(input)
+    @lives += -1
+    display_tries(@tried_letters)
+  end
+end
+
+def over?
+  @guess == @key || @lives <= 0
+end
+
+def display_tries(input)
+  @display = []
+  input.each do |letter|
+    if @key.include?(letter)
+      @display.push(letter.green)
+    else 
+      @display.push(letter.red)
+    end
+    puts @display.join(' ')
+  end
 end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,8 @@
 require_relative 'color'
 require_relative 'hangman'
 require_relative 'game'
+require 'yaml'
 
-def start
   puts "Hangman. Would you like to:"
   puts "1 - Start a new game"
   puts "2 - Load game"
@@ -10,16 +10,36 @@ def start
   while newgamechoice = gets.chomp.to_i
     case newgamechoice
     when 1
-      Game.new
+      game = Game.new
       break
     when 2
-      puts 'Load game'
+      game = load_game()
       break
     else 
       puts "Invalid input. Please try again, use 1 or 2"
       print '> '
     end
   end
+
+def save_game(game)
+  puts 'Enter file name to save the game'
+  filename = gets.chomp
+  savefile = File.open("../saved/#{filename}", 'w') {|f| f.write(YAML.dump(game)) }
 end
 
-start()
+def load_game
+
+
+end
+
+until game.over?
+  if game.player_choice == 'save'
+    if save_game(game)
+      puts "Your game has been saved. Thanks for playing!"
+      break
+    end
+  end
+end
+
+
+
